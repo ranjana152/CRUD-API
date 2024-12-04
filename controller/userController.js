6279d92bd7b58135d41fd6f9ee293f305ac84c2f
ranjana152 <ranjana.ravindran22@gmail.com>
2024-12-04 20:42:34 +0530

    Controller updated

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,4 @@
-const userModel = require("../database/psql.js")
-
+const {userModel} = require("../database/psql.js")
 module.exports.studentDetails=async(req,res)=>{
     try{
         const users=await userModel.findAll();
@@ -18,10 +17,37 @@ module.exports.newStudent=async(req,res)=>{
     try{
         const stud=await userModel.findOne({where:{rollId:rollId}});
         if(stud==null){
+            await userModel.create(req.body);
             return res.status(200).json({message:"Added successfully"});
         }
-        return res.status(200).json({message:"Already available"});
-        
+        return res.status(200).json({message:"Already available"});   
+    }
+    catch(error){
+        console.log(error);
+        return res.status(404).json({"error":"Error occured"});
+    }
+}
+module.exports.updateStud=async(req,res)=>{
+    let roll=req.params.rollId;
+    const{rollId,firstName,middleName,lastName,totalMarks}=req.body;
+    try{
+        const modify=await userModel.update(req.body,{where:{rollId}});
+        return res.status(200).json({message:"updated successfully"});   
+    }
+    catch(error){
+        console.log(error);
+        return res.status(404).json({"error":"Error occured"});
+    }
+}
+module.exports.deleteStud=async(req,res)=>{
+    let rollId=req.params.rollId;
+    const toDelete=await userModel.findOne({where:{rollId:rollId}});
+    try{
+        if(toDelete==null){
+            return res.status(200).json({message:"No data to delete"});  
+        }
+        await toDelete.destroy();
+        return res.status(200).json({message:"deleted successfully"});   
     }
     catch(error){
         console.log(error);
